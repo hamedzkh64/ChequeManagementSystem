92b5008e5f9cd26297a0337cfa394586fb5b846e
hamedzkh64 <153188354+hamedzkh64@users.noreply.github.com>
2024-11-01 11:52:11 +0330

    Update cheque.py

--- models/cheque.py
+++ models/cheque.py
@@ -1,18 +1,26 @@
-# cheque_management/models/cheque.py
-from odoo import models, fields, api
+# models/cheque.py
+
+from odoo import models, fields
 
 class Cheque(models.Model):
-    _name = 'cheque.management.cheque'
+    _name = 'cheque_management.cheque'
     _description = 'Cheque Management'
 
-    number = fields.Char(string="Cheque Number", required=True)
-    saad_number = fields.Char(string="Saad Number")
+    check_number = fields.Char(string="Check Number", required=True)
+    saad_number = fields.Char(string="Saad Number", required=True)
     amount = fields.Float(string="Amount", required=True)
     issue_date = fields.Date(string="Issue Date", required=True)
     maturity_date = fields.Date(string="Maturity Date", required=True)
     status = fields.Selection([
-        ('draft', 'Draft'),
+        ('in_vault', 'In Vault'),
         ('issued', 'Issued'),
+        ('endorsed', 'Endorsed'),
+        ('transferred', 'Transferred'),
         ('cleared', 'Cleared'),
         ('returned', 'Returned')
-    ], default='draft', string="Status")
+    ], default='in_vault', string="Status")
+    customer_id = fields.Many2one('res.partner', string="Customer", domain="[('customer_rank', '>', 0)]", required=True)
+    issuer_name = fields.Char(string="Issuer's Name")
+    issuer_national_id = fields.Char(string="Issuer's National ID")
+    category_id = fields.Many2one('cheque_management.category', string="Category", required=True)
+    checkbook_id = fields.Many2one('cheque_management.checkbook', string="Checkbook")
